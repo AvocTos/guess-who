71993fe78de5e44f70797974fc5aa80e38e27dc6
salt developer <salt-dev@DEV036.local>
2022-04-05 13:58:11 +0200

    added try catch blocks

--- server/index.js
+++ server/index.js
@@ -17,35 +17,59 @@ app.get('/api/users', async (req, res) => {
 });
 
 app.get('/api/scoreboard', async (req, res) => {
-    const results = await db.getScoreBoard();
-    res.send(results);
+    try {
+        const results = await db.getScoreBoard();
+        res.send(results);
+    } catch(err) {
+        console.log(err.message);
+    }
 });
 
 app.get('/api/user/:name', async (req, res) => {
-    const results = await db.findUser(req.params.name);
-    console.log('name', results);
-    return res.json(results);
+    try {
+        const results = await db.findUser(req.params.name);
+        console.log('name', results);
+        return res.json(results);
+    } catch(err) {
+        console.log(err.message);
+    }
 });
 
 app.get('/api/user/id/:sessionId', async (req, res) => {
-    const results = await db.findSessionId(req.params.sessionId);
-    return res.json(results);
+    try {
+        const results = await db.findSessionId(req.params.sessionId);
+        return res.json(results);
+    } catch(err) {
+        console.log(err.message);
+    }
 });
 
 app.post('/api/users', async (req, res) => {
-    const user = req.body;
-    const results = await db.addUser(user);
-    res.json(results);
+    try {
+        const user = req.body;
+        const results = await db.addUser(user);
+        res.json(results);
+    } catch(err) {
+        console.log(err.message);
+    }
 });
 
 app.put('/api/user/:name', async (req, res) => {
-    await db.updateUser(req.body.sessionId, req.params.name);
-    res.end();
+    try {
+        await db.updateUser(req.body.sessionId, req.params.name);
+        res.end();
+    } catch(err) {
+        console.log(err.message);
+    }
 });
 
 app.put('/api/user/score/:name', async (req, res) => {
-    await db.updatePoints(req.body.score, req.params.name);
-    res.end();
+    try {
+        await db.updatePoints(req.body.score, req.params.name);
+        res.end();
+    } catch(err) {
+        console.log(err.message);
+    }
 });
 
 app.get('/', (req, res) => {
